afbbc5b2d82ea4a1e52c6d7fe51c1fe970caa0b1
Katarzyna Bialas (PGS Software) <kbialas@pgs-soft.com>
2018-10-19 15:54:36 +0200

    first commit

---
+++ SignalRChat/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+
+//https://docs.microsoft.com/pl-pl/aspnet/signalr/overview/getting-started/tutorial-getting-started-with-signalr
+
+namespace SignalRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+    }
+}
